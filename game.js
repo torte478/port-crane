966a49e2c8e80349dd8a766f0e0c46d06a9e93c0
Vsevolod Kvachev <rasielll@gmail.com>
2016-05-27 23:49:55 +0300

    fixed timeslots

--- game.js
+++ game.js
@@ -217,6 +217,7 @@ GameStates.Game.prototype = {
     update: function () {
         updateWindSpeed()
         var data = getData(this.targetSlotX, this.targetSlotZ)
+        console.log('lol ' + this.targetSlotX)
 
 
         var trunc = function (s) {
@@ -232,9 +233,6 @@ GameStates.Game.prototype = {
         this.deck.y = data.deckHeight
         this.target.x = data.targetX + cw / 2
         this.target.y = data.targetY
-
-        this.targetSlotX = 0
-        this.targetSlotZ = 0
 
         // create minimap
         var slotsX = 5
