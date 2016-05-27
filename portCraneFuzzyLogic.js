fe80530dc475442b35b50b1c07e5cc1b5cc2e9eb
torte <torte478@gmail.com>
2016-05-27 18:16:53 +0300

    add: elastic movement

--- portCraneFuzzyLogic.js
+++ portCraneFuzzyLogic.js
@@ -8,28 +8,36 @@ function PortCraneFuzzyLogic(){
     //=====================
     var verticalMovement = new FuzzyLogicSystem();
 
-    //ВХОДНЫЕ ПАРАМЕТРЫ
-    //Расстояние до палубы
-    verticalMovement.addInputSet(0, "Close", new FuzzyInterval(0, 0, 3, 4));
-    verticalMovement.addInputSet(0, "Far", new FuzzyInterval(3, 4, 500, 500));
-    //Скорость спуска груза
+    //INPUT PARAMETERS
+    //Distance to ship
+    verticalMovement.addInputSet(0, "Close", new FuzzyInterval(0, 0, 10, 20));
+    verticalMovement.addInputSet(0, "Normal", new FuzzyInterval(10, 20, 250, 300));
+    verticalMovement.addInputSet(0, "Far", new FuzzyInterval(250, 300, 500, 500));
+    //Vertical speed
     verticalMovement.addInputSet(1, "Slow", new FuzzyInterval(0, 0, 1, 2));
-    verticalMovement.addInputSet(1, "Fast", new FuzzyInterval(1, 2, 4, 4));
+    verticalMovement.addInputSet(1, "Normal", new FuzzyNumber(1, 2, 3));
+    verticalMovement.addInputSet(1, "Fast", new FuzzyInterval(2, 3, 4, 4));
 
-    //ВЫХОДНЫЕ ПАРАМЕТРЫ
-    verticalMovement.addOutputSet("Reduce", new FuzzyInterval(-1, -1, -0.5, 0));
-    verticalMovement.addOutputSet("Nothing", new FuzzyNumber(-0.5, 0, 0.5));
-    verticalMovement.addOutputSet("Increase", new FuzzyInterval(0, 0.5, 1, 1));
+    //OUTPUT PARAMETERS
+    verticalMovement.addOutputSet("Reduce", new FuzzyInterval(-1, -1, -0.2, 0));
+    verticalMovement.addOutputSet("Nothing", new FuzzyNumber(-0.2, 0, 0.2));
+    verticalMovement.addOutputSet("Increase", new FuzzyInterval(0, 0.2, 1, 1));
 
-    //ПРАВИЛА
-    //==========Расстояние до палубы===Скорость спуска груза=====
+    //RULES
+    //==========Distance to ship ===Vertical speed=====
     verticalMovement.setRule(["Close", "Slow"], "Nothing");
+    verticalMovement.setRule(["Close", "Normal"], "Reduce");
     verticalMovement.setRule(["Close", "Fast"], "Reduce");
     //----------------------------------------------------
+    verticalMovement.setRule(["Normal", "Slow"], "Increase");
+    verticalMovement.setRule(["Normal", "Normal"], "Nothing");
+    verticalMovement.setRule(["Normal", "Fast"], "Reduce");
+    //----------------------------------------------------
     verticalMovement.setRule(["Far",   "Slow"], "Increase");
+    verticalMovement.setRule(["Far",   "Normal"], "Increase");
     verticalMovement.setRule(["Far",   "Fast"], "Nothing");
 
-    //ТОЧНОСТЬ
+    //ACCURACY
     verticalMovement.setAccuracy(100);
 
     this.verticalMovement = verticalMovement;
@@ -39,42 +47,43 @@ function PortCraneFuzzyLogic(){
     //=====================
     var horizontalMovement = new FuzzyLogicSystem();
 
-    //ВХОДНЫЕ ПАРАМЕТРЫ
-    //Смещение от цели по X
+    //INPUT PARAMETERS
+    //parallax
     horizontalMovement.addInputSet(0, "To left", new FuzzyInterval(-5, -5, -2, -1));
     horizontalMovement.addInputSet(0, "No chng", new FuzzyNumber(-2, 0, 2));
     horizontalMovement.addInputSet(0, "To right", new FuzzyInterval(1, 2, 5, 5));
-    //Скорость контейнера по Х
+    //Horizontal speed
     horizontalMovement.addInputSet(1, "Move left", new FuzzyInterval(-5, -5, -2, -1));
     horizontalMovement.addInputSet(1, "No move", new FuzzyNumber(-2, 0, 2));
     horizontalMovement.addInputSet(1, "Move right", new FuzzyInterval(1, 2, 5, 5));
 
-    //ВЫХОДНЫЕ ПАРАМЕТРЫ
+    //OUTPUT PARAMETERS
     horizontalMovement.addOutputSet("Move left", new FuzzyInterval(-1, -1, -0.5, 0));
     horizontalMovement.addOutputSet("Nothing", new FuzzyNumber(-0.5, 0, 0.5));
     horizontalMovement.addOutputSet("Move right", new FuzzyInterval(0, 0.5, 1, 1));
 
-    //ПРАВИЛА
-    //==========================Смещение====Контейнер=========
+    //RULES
+    //==========================Parallax====Speed=========
     horizontalMovement.setRule(["To left" , "Move left" ], "Move right");
     horizontalMovement.setRule(["To left" , "No move"   ], "Move right");
     horizontalMovement.setRule(["To left" , "Move right"], "Nothing");
-    //==========================Смещение====Контейнер=========
+    //==========================Parallax====Speed=========
     horizontalMovement.setRule(["No chng" , "Move left" ], "Move right");
     horizontalMovement.setRule(["No chng" , "No move"   ], "Nothing");
     horizontalMovement.setRule(["No chng" , "Move right"], "Move left");
-    //==========================Смещение====Контейнер========
+    //==========================Parallax====Speed========
     horizontalMovement.setRule(["To right", "Move left" ], "Nothing");
     horizontalMovement.setRule(["To right", "No move"   ], "Move left");
     horizontalMovement.setRule(["To right", "Move right"], "Move left");
 
-    //ТОЧНОСТЬ
+    //ACCURACY
     horizontalMovement.setAccuracy(100);
 
     this.horizontalMovement = horizontalMovement;
 }
 
 PortCraneFuzzyLogic.prototype.getVerticalMovement = function(distToShip, containerSpeedY){
+    // console.log("speedY: " + containerSpeedY);
     return this.verticalMovement.calc([distToShip, containerSpeedY]);
 }
 
